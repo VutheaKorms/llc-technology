4f1bfa0474cb766b044e00ce8e3ccdb7fd36faff
So Thea <vutheakorm168@gmail.com>
2017-07-05 15:23:44 +0700

    added proejct

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::resource('brands','BrandsController');
+Route::put('brands/disable/{id}','BrandsController@disable');
+Route::get('brands/status/{status}','BrandsController@getAllActive');
+
+Route::resource('categories','CategoriesController');
+Route::put('categories/disable/{id}','CategoriesController@disable');
+Route::get('categories/status/{status}','CategoriesController@getAllActive');
+
+Route::group(['middleware' => ['web']], function () {
+    Route::resource('products','ProductsController');
+    Route::put('products/disable/{id}','ProductsController@disable');
+});
+
+Route::post('test/upload','ProductsController@upload');
+Route::get('products/review/{proId}','ProductsController@ReviewProduct');
